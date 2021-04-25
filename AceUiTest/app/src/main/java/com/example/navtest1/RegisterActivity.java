7318b576e91d85f35b6465ab1178868b71f28d01
Marcus Wong <marcustylerwong0@gmail.com>
2021-04-25 05:23:41 -0400

    commit

--- AceUiTest/app/src/main/java/com/example/navtest1/RegisterActivity.java
+++ AceUiTest/app/src/main/java/com/example/navtest1/RegisterActivity.java
@@ -1,39 +1,16 @@
 package com.example.navtest1;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.rengwuxian.materialedittext.MaterialEditText;
-
-import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
-
-    MaterialEditText username, email, password;
-    Button btn_register;
-
-    FirebaseAuth auth;
-    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+<<<<<<< HEAD
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -106,5 +83,7 @@ public class RegisterActivity extends AppCompatActivity {
                         }
                     }
                 });
+=======
+>>>>>>> parent of cd4fd38 (Add Login and Register Functions)
     }
 }
